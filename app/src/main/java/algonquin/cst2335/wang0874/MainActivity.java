ac504af5b2f648888768dc9b2d519916db2ec69b
Xihua Wang <wang0874@algonquinlive.com>
2022-09-12 12:15:44 -0400

    Second commit: changed the icon and app colours

--- app/src/main/java/algonquin/cst2335/wang0874/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/wang0874/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //loads the XML file on screen;
         setContentView(R.layout.activity_main);
     }
 }
