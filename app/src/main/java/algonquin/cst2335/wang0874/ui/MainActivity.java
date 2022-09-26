f64d269b57965c72ed6d36fd6536d0bbcdd6e3c0
Xihua Wang <wang0874@algonquinlive.com>
2022-09-26 15:53:09 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/wang0874/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/wang0874/ui/MainActivity.java
@@ -4,25 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import algonquin.cst2335.wang0874.data.MainViewModel;
+import algonquin.cst2335.wang0874.R;
+// import algonquin.cst2335.wang0874.data.MainViewModel;
 import algonquin.cst2335.wang0874.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding variableBinding;
+
+//    private ActivityMainBinding variableBinding;
+
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.spin_switch);
 
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-
-
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
 
 
     }}
