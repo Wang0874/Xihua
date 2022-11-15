25a2f2a69cfc77e4d5baa4f9d5dae3a3e7beeb7a
Xihua Wang <wang0874@algonquinlive.com>
2022-11-15 13:34:47 -0500

    Finished Week 8-2

--- app/src/main/java/algonquin/cst2335/wang0874/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/wang0874/ChatRoom.java
@@ -1,7 +1,6 @@
 package algonquin.cst2335.wang0874;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
